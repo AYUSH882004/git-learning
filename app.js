94953a9ec41058fee465b47a5a1f31a594ae36fb
Ayush Sahu <ayushsahu882004@gmail.com>
2025-01-09 21:43:13 +0530

    fourth commit

--- app.js
+++ app.js
@@ -6,3 +6,15 @@ function update(value) {
     balance+=value;
 }
 update(200);
+
+const x='ayush';
+console.log(x+"aryan");
+//git log --oneline
+const faltu="hello ";
+console.log(faltu);//now i want to delete these commit  (git reset --hard code)
+ //code which i got from git log --oneline
+
+
+//  console.log("i am learning git and git hub");
+ 
+ //now i want to remove 4th commit
