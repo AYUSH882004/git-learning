564f3c300bfd3aacbd0d1795db1b0f14d94c5eed
Ayush Sahu <ayushsahu882004@gmail.com>
2025-01-09 21:45:54 +0530

    fifth commit

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ console.log(faltu);//now i want to delete these commit  (git reset --hard code)
  //code which i got from git log --oneline
 
 
-//  console.log("i am learning git and git hub");
+ console.log("i am learning git and git hub");
+ console.log("i am pro");
  
  //now i want to remove 4th commit
