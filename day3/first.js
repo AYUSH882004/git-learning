275c93f1420141d1d4492f06f4529c155bdf0bac
Ayush Sahu <ayushsahu882004@gmail.com>
2025-01-10 21:38:05 +0530

    master changese

--- day3/first.js
+++ day3/first.js
@@ -5,11 +5,8 @@ console.log(button)
 const login="login page added";
 console.log(login);
 
-const payment="integrated the payment gateway"
-console.log(payment);
-
-const upi="upi added";
-console.log(upi);
+const footer="footer added";
+console.log(footer);
 //git checkout master/payment/feature
 //git log --oneline
 
